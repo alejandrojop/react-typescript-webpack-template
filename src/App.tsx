8e851f8d4e05587b8272cdbf5ccdabd62d90f361
alejandrojop <alejandro.pacheco@acin.pt>
2021-11-10 12:48:15 +0000

    prettier fixing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { Counter } from './Counter';
-import './styles.css';
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
 
 export const App = () => {
+  const [display, setDisplay] = useState(true);
+  console.log(display);
+  const visible = ['Display', 'DisplayOF'];
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>
-      <Counter />
+      <h1 id="title" className="title">
+        oishdg+asdh+fhdgsnd+gh
+      </h1>
+      <Button data-testid="custom" onClick={() => setDisplay(!display)}>
+        {display ? visible[1] : visible[0]}
+      </Button>
+      {display && <h1>mini test</h1>}
     </>
   );
 };
